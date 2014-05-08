3d6cb6d1b5a5b04f36c6cffcc3278d377ec5b830
Your Name <come0220@gmail.com>
2014-05-08 21:51:54 +0900

    Revert "UP"
    This reverts commit 4deb612e8dd3ddd158840f615a28a103197b7fa6.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,11 @@ class ApplicationController < ActionController::Base
     end    
   end
 
-#  def authenticate
-#  authenticate_or_request_with_http_basic do |user_name, password|
-#    user_name == 'admin' && password == 'password'
-#  end
-#  end
+  def authenticate
+  authenticate_or_request_with_http_basic do |user_name, password|
+    user_name == 'admin' && password == 'password'
+  end
+  end
 
   def test
   end
